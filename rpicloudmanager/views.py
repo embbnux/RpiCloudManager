934b7a068d0d77770653effff18d818b4c91101a
emrpi <embbnux@embbnux.com>
2014-10-07 11:14:11 +0000

    raspberry pi gpio

--- rpicloudmanager/views.py
+++ rpicloudmanager/views.py
@@ -6,7 +6,22 @@ from flask import Flask, request, session, g, redirect, url_for, abort, render_t
 from contextlib import closing
 from rpicloudmanager import app
 import re
-
+import RPi.GPIO as GPIO
 @app.route('/')
 def show_index():
   return render_template('home.html')
+
+@app.route('/gpio/<int:id>',methods=['POST'])
+def gpio_led(id):
+  if request.method == 'POST':
+    GPIO.setmode(GPIO.BOARD)
+    if id<100:
+      GPIO.setup(id,GPIO.OUT)
+      GPIO.setmode(GPIO.BOARD)
+      GPIO.setup(id,GPIO.OUT)
+      GPIO.output(id,False)
+    else:
+      GPIO.setup(id-100,GPIO.OUT)
+      GPIO.output(id-100,True)
+  return redirect(url_for('show_index'))
+
